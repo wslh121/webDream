9b3da08a320f80a1ca34fd4b5f6534d620f0c46e
lihang <wslh121@163.com>
2019-01-31 16:11:20 +0800

    create project

---
+++ disconvery/src/main/java/com/wslh/disconvery/DisconveryApplication.java
@@ -0,0 +1,14 @@
+package com.wslh.disconvery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisconveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisconveryApplication.class, args);
+    }
+
+}
+
